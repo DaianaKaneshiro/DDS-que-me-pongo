1f600038351146776884a864bf7b6eeb672fdf60
Daiana Kaneshiro <daiana.kane@gmai.com>
2022-04-22 00:33:26 -0300

    categoria

--- src/main/java/prenda.java
+++ src/main/java/prenda.java
@@ -1,4 +1,61 @@
+import java.util.*;
 
-public class prenda {
-//probando
+
+
+class Prenda {
+ TipoDePrenda tipo;
+ Material material;
+ Color color;
+ 
+ public Prenda(TipoDePrenda tipo, Material material, Color color) {
+	this.tipo=tipo;
+	this.material=material;
+	this.color=color;
+ 	
+ }
+}
+
+
+class TipoDePrenda{
+	Categoria categoria;
+
+}
+ enum Material{
+	ALGODON,
+	MODAL,
+	LYCRA,
+	VIDRIO,
+	CUERO,
+	JEAN,
+	
+}
+
+class Color{
+	Integer rojo;
+	Integer azul;
+	Integer amarillo;
+	
+	
+}
+
+// Composicion/ Interface 
+
+public interface Categoria{
+	
+
+}
+enum ParteSuperior implements Categoria{
+	REMERA, PULOVER,CAMISA
+}
+
+enum ParteInferior implements Categoria{
+	PANTALON
+}
+
+enum Calzado implements Categoria{
+	CONVERSE
+}
+
+enum Accesorio implements Categoria{
+	ANTEOJOS, COLLAR, AROS
 }
